2f4e7865f75a3e345cef05db1174a3072a56c319
Pedro Miguel <pedromrap@gmail.com>
2023-01-24 17:17:15 -0300

    feat: criação componente de barra de progresso

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export const ProgressBar = ({ progress }: ProgressBarProps) => {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        className="h-3 bg-violet-600 rounded-xl transition-all"
+        style={{ width: `${progress}%` }}
+      />
+    </div>
+  );
+};
