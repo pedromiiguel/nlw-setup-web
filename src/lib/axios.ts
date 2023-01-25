0d5c3dcacad619581d3be961dccf02a6f6b055e5
Pedro Miguel <pedromrap@gmail.com>
2023-01-24 21:04:13 -0300

    chore: corrrecao conflito

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: import.meta.env.VITE_API_URL
-=======
-  baseURL: import.meta.env.VITE_BASE_URL
->>>>>>> 180f3c8ed0f7dd16fc3c7038e44b1f203a72f6b9
 });
